b7fb21dbdb7c4dc4014cd3829bc3ecd145be3c31
liangyongchen <375427684@qq.com>
2018-03-20 16:14:05 +0800

    添加测试界面

--- app/src/main/java/com/zhy/stickynavlayout/MainActivity.java
+++ app/src/main/java/com/zhy/stickynavlayout/MainActivity.java
@@ -1,11 +1,13 @@
 package com.zhy.stickynavlayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,93 +15,87 @@ import android.widget.TextView;
 import com.zhy.stickynavlayout.view.SimpleViewPagerIndicator;
 
 
-public class MainActivity extends FragmentActivity
-{
-	private String[] mTitles = new String[] { "简介", "评价", "相关" };
-	private SimpleViewPagerIndicator mIndicator;
-	private ViewPager mViewPager;
-	private FragmentPagerAdapter mAdapter;
-	private TabFragment[] mFragments = new TabFragment[mTitles.length];
+public class MainActivity extends FragmentActivity {
+    private String[] mTitles = new String[]{"简介", "评价", "相关","天才"};
+    private SimpleViewPagerIndicator mIndicator;
+    private ViewPager mViewPager;
+    private FragmentPagerAdapter mAdapter;
+    private TabFragment[] mFragments = new TabFragment[mTitles.length];
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		initViews();
-		initDatas();
-		initEvents();
-	}
+        initViews();
+        initDatas();
+        initEvents();
+    }
 
-	private void initEvents()
-	{
-		mViewPager.setOnPageChangeListener(new OnPageChangeListener()
-		{
-			@Override
-			public void onPageSelected(int position)
-			{
-			}
+    private void initEvents() {
+        mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+            }
 
-			@Override
-			public void onPageScrolled(int position, float positionOffset,
-					int positionOffsetPixels)
-			{
-				mIndicator.scroll(position, positionOffset);
-			}
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+                mIndicator.scroll(position, positionOffset);
+            }
 
-			@Override
-			public void onPageScrollStateChanged(int state)
-			{
+            @Override
+            public void onPageScrollStateChanged(int state) {
 
-			}
-		});
+            }
+        });
 
-	}
+    }
 
-	private void initDatas()
-	{
-		mIndicator.setTitles(mTitles);
+    private void initDatas() {
+        mIndicator.setTitles(mTitles);
 
-		for (int i = 0; i < mTitles.length; i++)
-		{
-			mFragments[i] = (TabFragment) TabFragment.newInstance(mTitles[i]);
-		}
+        for (int i = 0; i < mTitles.length; i++) {
+            mFragments[i] = (TabFragment) TabFragment.newInstance(mTitles[i]);
+        }
 
-		mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
-		{
-			@Override
-			public int getCount()
-			{
-				return mTitles.length;
-			}
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public int getCount() {
+                return mTitles.length;
+            }
 
-			@Override
-			public Fragment getItem(int position)
-			{
-				return mFragments[position];
-			}
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
 
-		};
+        };
 
-		mViewPager.setAdapter(mAdapter);
-		mViewPager.setCurrentItem(0);
-	}
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setCurrentItem(0);
+    }
 
-	private void initViews()
-	{
-		mIndicator = (SimpleViewPagerIndicator) findViewById(R.id.id_stickynavlayout_indicator);
-		mViewPager = (ViewPager) findViewById(R.id.id_stickynavlayout_viewpager);
+    private void initViews() {
+        mIndicator = (SimpleViewPagerIndicator) findViewById(R.id.id_stickynavlayout_indicator);
+        mViewPager = (ViewPager) findViewById(R.id.id_stickynavlayout_viewpager);
 
 
-		LinearLayout ll = (LinearLayout) findViewById(R.id.id_stickynavlayout_topview);
-		TextView tv = new TextView(this);
-		tv.setText("我的动态添加的");
-		tv.setBackgroundColor(0x77ff0000);
-		ll.addView(tv, new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.MATCH_PARENT, 600));
+        LinearLayout ll = (LinearLayout) findViewById(R.id.id_stickynavlayout_topview);
+        final TextView tv = new TextView(this);
+        tv.setText("我的动态添加的");
+        tv.setBackgroundColor(0x77ff0000);
+        ll.addView(tv, new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, 600));
 
-	}
+        ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
+
+    }
 
 
 }
--- app/src/main/java/com/zhy/stickynavlayout/view/SimpleViewPagerIndicator.java
+++ app/src/main/java/com/zhy/stickynavlayout/view/SimpleViewPagerIndicator.java
@@ -16,7 +16,7 @@ public class SimpleViewPagerIndicator extends LinearLayout
 {
 
 	private static final int COLOR_TEXT_NORMAL = 0xFF000000;
-	private static final int COLOR_INDICATOR_COLOR = Color.GREEN;
+	private static final int COLOR_INDICATOR_COLOR = Color.RED;
 
 	private String[] mTitles;
 	private int mTabCount;
